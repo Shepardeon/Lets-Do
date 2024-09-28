ad223cbec727e46b1cf18cde996e9087c31f79f3
Shepardeon <thomas.demaen@life-sensation.fr>
2024-09-28 19:24:14 +0200

    reset

--- back/src/api/middlewares/notFound.ts
+++
@@ -1,5 +0,0 @@
-import { NextFunction, Request, Response } from 'express'
-
-export const notFound = (req: Request, res: Response, next: NextFunction) => {
-  return res.status(404).json({ message: 'Not Found!' })
-}
